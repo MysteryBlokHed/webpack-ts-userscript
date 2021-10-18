546881d3f605647798a46cde295e0bb627859aaa
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-10-18 18:27:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# webpack-ts-userscript
+
+A template to create UserScripts using TypeScript and Webpack.
+
+<!-- Info on how to use the template -->
+
+## Customizing the template
+
+### Changing UserScript info
+
+The UserScript's name, description, version, and author are all retrieved from the project's `package.json`.
+Anything else such as the site to match and the grants are changed from the [Webpack Config].
+
+<!-- These instructions can be updated to fit your project's requirements -->
+
+## Building
+
+### Setup
+
+Building this project requires Node.js and Yarn.
+To install dependencies, run:
+
+```sh
+yarn install
+```
+
+### Building files
+
+To build the project, run:
+
+```sh
+yarn build
+```
+
+To automatically build when a source file is modified, run:
+
+```sh
+yarn dev
+```
+
+This will output files in the `build/` directory, including `webpack-ts-userscript.user.js`, which is the UserScript.
+Opening this file in your browser with a UserScript extension installed should prompt you to install it.
+
+## License
+
+This template is licensed under either of
+
+- Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+  <http://www.apache.org/licenses/LICENSE-2.0>)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or
+  <http://opensource.org/licenses/MIT>)
+
+at your option.
+
+[webpack config]: webpack.config.js
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
+const webpack = require('webpack')
+const package = require('./package.json')
+
+/** The UserScript info to add to the top of the outputted file */
+const banner = `
+// ==UserScript==
+// @name        ${package.name}
+// @description ${package.description}
+// @version     ${package.version}
+// @author      ${package.author}
+// @match       *://example.com/*
+// @grant       none
+// ==/UserScript==
+`
+
+/** The name of the generated Userscript file */
+const outFile = `${package.name}.user.js`
+
+console.log(__dirname)
+
+module.exports = {
+  entry: './build/index.js',
+  output: {
+    filename: outFile,
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner,
+      raw: true,
+      entryOnly: true,
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          format: {
+            comments(_, comment) {
+              const userScriptComment = /==\/?UserScript==|@/g
+              return (
+                comment.type === 'comment1' &&
+                userScriptComment.exec(comment.value)
+              )
+            },
+          },
+        },
+      }),
+    ],
+  },
+  mode: 'production',
+}
